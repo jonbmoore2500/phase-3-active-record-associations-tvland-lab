6d88fe497e6859ecfcb1d7931fed326f1f291ef7
Jonathan <jonathanmoorevla@gmail.com>
2022-12-17 13:14:55 -0600

    commit tree 84ccfd1f01f54bfecdf48272cf582de5c7f6cfdc

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        actor_arr = []
+        for actor in self.actors do
+            actor_arr << actor.full_name
+        end
+        return actor_arr
+    end
+
 end
