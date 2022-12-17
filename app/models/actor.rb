19684c3e21c522ebab62b842148c78ff9f080afc
Jonathan <jonathanmoorevla@gmail.com>
2022-12-17 13:15:19 -0600

    Build tables, models, methods. pass all tests

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        return "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        rolesArr = []
+        for role in self.characters do
+            rolesArr << "#{role.name} - #{role.show.name}"
+        end
+        return rolesArr
+    end
+
+
 end
